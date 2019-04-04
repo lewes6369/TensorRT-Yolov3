9888f08a3674a4ed343f508da10e08a12ab6d059
Yao Wei <240447420@qq.com>
2019-04-04 12:25:02 +0800

    same thing i did

--- include/configs.h
+++ include/configs.h
@@ -8,10 +8,12 @@ namespace Tn
     const std::string INPUT_PROTOTXT ="yolov3_trt.prototxt";
     const std::string INPUT_CAFFEMODEL = "yolov3.caffemodel";
     const std::string INPUT_IMAGE = "test.jpg";
+	const std::string CLASS_NAME = "coco.name";
     const std::string EVAL_LIST = "";
     const std::string CALIBRATION_LIST = "";
     const std::string MODE = "fp32";
     const std::string OUTPUTS= "yolo-det";//layer82-conv,layer94-conv,layer106-conv
+	const int DISPLAY = 1;
     const int INPUT_WIDTH = 608;
     const int INPUT_HEIGHT = 608;
 
